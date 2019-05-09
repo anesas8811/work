a7bf425e3bcf928fbed82cbf91bbd833c27ddd18
anesas8811 <50436737+anesas8811@users.noreply.github.com>
2019-05-09 12:10:36 +0530

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -13,7 +13,9 @@ module.exports = async function (req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
         } else {
+            while(1){
             const file = await getScreenshot(url, type, qual, fullPage);
+            }
             res.statusCode = 200;
             res.setHeader('Content-Type', `image/${type}`);
             res.end(file);
